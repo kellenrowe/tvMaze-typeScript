d9dc6d49de7b2c364d1c4bb0a379d48b60853b2f
kellenrowe <kellenrowe@gmail.com>
2021-02-01 16:16:41 -0800

    converted getshowsbyterm function to ts

--- tvmaze.ts
+++ tvmaze.ts
@@ -1,10 +1,20 @@
 import axios from "axios"
 import * as $ from 'jquery';
 
+const BASE_URL: string = "http://api.tvmaze.com/"
+const DEFAULT_IMG: string =
+  "https://www.southwestjournal.com/wp-content/uploads/2018/11/shutterstock_1075355216.jpg"
+
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+type Show = {
+  id: number,
+  name: string,
+  summary: string,
+  image: string
+}
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -12,27 +22,18 @@ const $searchForm = $("#searchForm");
  *    Each show object should contain exactly: {id, name, summary, image}
  *    (if no image URL given by API, put in a default image URL)
  */
+async function getShowsByTerm(term: string): Promise<Show[]> {
 
-async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  let response = await axios.get(`${BASE_URL}search/shows?q=${term}`);
+
+  let shows: Show[] = response.data.map(s => ({
+    id: s.show.id,
+    name: s.show.name,
+    summary: s.show.summary,
+    image: s.show.image.medium || s.show.image.original || DEFAULT_IMG
+  }));
+  
+  return shows;
 }
 
 
